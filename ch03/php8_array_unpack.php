8e5df632f67add9fcfc5dbc3188c21f38d7fd63d
dbierer <doug@unlikelysource.com>
2020-12-02 16:05:49 +0700

    2020-12-02 DB: Adding examples of array warnings to error

---
+++ ch03/php8_array_unpack.php
@@ -0,0 +1,14 @@
+<?php
+// ch03/php8_array_unpack.php
+
+function add($a, $b) { return $a + $b; }
+
+// here's the data:
+$vals = [ [18,48], [72,99], [11,37] ];
+
+// we can unpack the array using "...":
+foreach ($vals as $pair) {
+    echo 'The sum of ' . implode(' + ', $pair) . ' is ';
+    echo add(...$pair);
+    echo "\n";
+}
---
+++ ch03/php8_warn_array_unpack.php
@@ -0,0 +1,14 @@
+<?php
+// ch03/php8_warn_array_unpack.php
+
+$alpha = range('A','Z');
+// produces the last letter
+echo array_pop($alpha);
+
+// flatten the array into a string
+$alpha = implode('', range('A','Z'));
+// produce the last letter
+echo $alpha[-1];
+
+// only array and Traversable can be unpacked
+echo array_pop($alpha);
