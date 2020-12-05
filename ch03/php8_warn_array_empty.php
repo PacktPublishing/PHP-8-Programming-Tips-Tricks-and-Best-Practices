c9aec7e7f03fad0446a762bd27272a45a76fb83e
dbierer <doug@unlikelysource.com>
2020-12-05 15:38:39 +0700

    2020-12-05 DB: Finished warnings to errors section

--- ch03/php8_warn_array_empty.php
+++ ch03/php8_warn_array_empty.php
@@ -1,7 +1,7 @@
 <?php
 // ch03/php8_warn_array_empty.php
 
-$obj    = new stdClass();
+$obj = new stdClass();
 $obj->c = 'C';
 // can't use an object as an array key
 $b = ['A' => 1, 'B' => 2, 'C' => 3];
