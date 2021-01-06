e95d18d75831549d489df000b1bc0bda14068ff4
dbierer <doug@unlikelysource.com>
2021-01-06 14:40:36 +0700

    2021-01-06 DB: Creating FFI examples

--- ch04/php8_ffi_cdef.php
+++ ch04/php8_ffi_cdef.php
@@ -6,10 +6,12 @@
 
 $key  = '';
 $size = 4;
-$seed = FFI::cdef('void srand (unsigned int seed);', 'libc.so.6');
-$rand = FFI::cdef('int rand (void);', 'libc.so.6');
-$seed->srand(random_int(0, 999));
+$code = <<<EOT
+    void srand (unsigned int seed);
+    int rand (void);
+EOT;
+$ffi = FFI::cdef($code, 'libc.so.6');
+$ffi->srand(random_int(0, 999));
 for ($x = 0; $x < $size; $x++)
-    $key .= sprintf('%x', $rand->rand());
+    $key .= sprintf('%x', $ffi->rand());
 echo "$key\n";
-
