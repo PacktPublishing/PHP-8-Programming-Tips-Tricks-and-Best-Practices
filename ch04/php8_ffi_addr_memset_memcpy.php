1a675ca845da228e109a71315bff30832e25477a
dbierer <doug@unlikelysource.com>
2021-01-18 08:50:22 +0700

    2021-01-18 DB: Added FFI::memcpy() example

--- ch04/php8_ffi_addr_memset_memcpy.php
+++ ch04/php8_ffi_addr_memset_memcpy.php
@@ -1,5 +1,5 @@
 <?php
-// ch04/php8_ffi_addr.php
+// ch04/php8_ffi_addr_memset_memcpy.php
 // create C data structure
 
 // define output function
@@ -14,8 +14,13 @@ $arr  = FFI::new(FFI::type("char[$size]"));
 FFI::memset($arr, 65, $size);
 echo $output($arr, $size);
 
+// create copy
+$arr2  = FFI::new(FFI::type("char[$size]"));
+FFI::memcpy($arr2, $arr, $size);
+echo $output($arr2, $size);
+
 // create a reference
 $ref = FFI::addr($arr);
 FFI::memset($ref[0], 66, 6);
 echo $output($arr, $size);
-var_dump($arr, $ref);
+var_dump($ref, $arr, $arr2);
