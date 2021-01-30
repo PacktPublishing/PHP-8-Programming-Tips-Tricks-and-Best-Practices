d45f4ea29e1bb3e7b22129d348a989424ad5412b
dbierer <doug@unlikelysource.com>
2021-01-30 16:10:39 +0700

    2021-01-30 DB: Started ch05 OOP BC breaks

---
+++ ch05/php8_bc_break_construct.php
@@ -0,0 +1,22 @@
+<?php
+// /repo/ch05/php8_oop_bc_break_construct.php
+
+class Text
+{
+    public $fh = '';
+    public const ERROR_FN = 'ERROR: file not found';
+    public function text(string $fn)
+    {
+        if (!file_exists($fn))
+            throw new Exception(self::ERROR_FN);
+        $this->fh = new SplFileObject($fn, 'r');
+    }
+    public function getText()
+    {
+        return $this->fh->fpassthru();
+    }
+}
+
+$fn   = __DIR__ . '/../sample_data/gettysburg.txt';
+$text = new Text($fn);
+echo $text->getText();
