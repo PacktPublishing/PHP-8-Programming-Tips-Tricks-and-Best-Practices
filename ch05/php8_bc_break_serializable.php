fb20f9ef9533fd3311427e15fd77610a0dde7124
dbierer <doug@unlikelysource.com>
2021-02-12 12:59:17 +0700

    2021-02-12 DB: Added serializable examples

--- ch05/php8_bc_break_serializable.php
+++ ch05/php8_bc_break_serializable.php
@@ -1,9 +1,8 @@
 <?php
-// /repo/ch05/php8_bc_break_serialization.php
+// /repo/ch05/php8_bc_break_serializable.php
 class A implements Serializable {
     private $a = 'A';
     private $b = 'B';
-    private $c = 'C';
     private $u = NULL;
     public function serialize() {
         $this->u = new DateTime();
@@ -15,7 +14,29 @@ class A implements Serializable {
             $this->$key = $val;
     }
 }
-$a = new A();
-$str = serialize($a);
-$b = unserialize($str);
-var_dump($a, $b);
+$a1 = new A();
+var_dump($a1);
+$str = serialize($a1);
+$a2 = unserialize($str);
+var_dump($a2);
+echo "\n";
+
+class B extends A {
+    private $c = 'C';
+    public function serialize() {
+        $this->c = time();
+        $parent = parent::serialize();
+        $child  = serialize($this->c);
+        return serialize([$parent, $child]);
+    }
+    public function unserialize($payload) {
+        [$parent, $child] = unserialize($payload);
+        parent::unserialize($parent);
+        $this->c = unserialize($child);
+    }
+}
+$old_b = new B();
+$str = serialize($old_b);
+echo $str . "\n";
+$new_b = unserialize($str);
+var_dump($new_b);
