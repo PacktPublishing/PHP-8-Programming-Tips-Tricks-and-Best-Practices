23be13cf19c6f833478d7f63dfef44a34546a2ab
dbierer <doug@unlikelysource.com>
2021-04-09 18:06:20 +0700

    2021-04-09 DB: Added XMLWriter example

---
+++ ch07/php8_xml_writer.php
@@ -0,0 +1,16 @@
+<?php
+// /repo/ch067/php8_xml_writer.php
+
+$xml = new XMLWriter();
+$xml->openMemory();
+$xml->startDocument('1.0', 'UTF-8');
+$xml->startElement('fruit');
+$xml->startElement('item');
+$xml->text('Apple');
+$xml->endElement();
+$xml->startElement('item');
+$xml->text('Banana');
+$xml->endElement();
+$xml->endElement();
+$xml->endDocument();
+echo $xml->outputMemory();
