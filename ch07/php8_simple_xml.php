a7bc0e73dfdf7924f8f9a1b5da36edfc4cedb3f0
dbierer <doug@unlikelysource.com>
2021-04-10 11:53:16 +0700

    2021-04-10 DB: Added SimpleXMLElement example

---
+++ ch07/php8_simple_xml.php
@@ -0,0 +1,17 @@
+<?php
+// /repo/ch067/php8_simple_xml.php
+
+$fn = __DIR__ . '/includes/tree.xml';
+$xml = simplexml_load_file($fn);
+$iter = new RecursiveIteratorIterator($xml, RecursiveIteratorIterator::SELF_FIRST);
+foreach ($iter as $branch) {
+    if (!empty($branch->descendent)) {
+        echo $branch->descendent;
+        echo ($branch->descendent['gender'] == 'F')
+             ? ', daughter of '
+             : ', son of ';
+        echo $branch['name'];
+        if (empty($branch->spouse)) echo "\n";
+        else echo ", married to {$branch->spouse}\n";
+    }
+}
