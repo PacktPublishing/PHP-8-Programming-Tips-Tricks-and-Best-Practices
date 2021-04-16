b33fadc5532872f3c171ab44f5f714f0a7676bb8
dbierer <doug@unlikelysource.com>
2021-04-16 12:22:42 +0700

    2021-04-16 DB: Finished reflection + database extensions sections

---
+++ ch07/php7_pdo_err_mode.php
@@ -0,0 +1,16 @@
+<?php
+// /repo/ch07/php7_pdo_err_mode.php
+
+$dsn = 'mysql:host=localhost;dbname=php8_tips';
+$usr = 'php8';
+$pwd = 'password';
+$pdo = new PDO($dsn, $usr, $pwd);
+$sql = 'SELEK propertyKey, hotelName FUM hotels '
+     . "WARE country = 'CA'";
+$stm = $pdo->query($sql);
+if ($stm) {
+    while($hotel = $stm->fetch(PDO::FETCH_OBJ))
+        echo $hotel->name . ' ' . $hotel->key . "\n";
+} else {
+    echo "No Results\n";
+}
