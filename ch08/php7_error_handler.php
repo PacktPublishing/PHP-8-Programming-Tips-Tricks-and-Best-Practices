f88058e056ae3028ebe17082ac3b617cc1d1b101
dbierer <doug@unlikelysource.com>
2021-05-10 13:17:00 +0700

    2021-05-10 DB: Finished chapter 8

---
+++ ch08/php7_error_handler.php
@@ -0,0 +1,21 @@
+<?php
+// /repo/ch08/php7_error_handler.php
+
+function handler($errno, $errstr, $errfile, $errline, $errcontext = NULL)
+{
+    echo "Number : $errno\n";
+    echo "String : $errstr\n";
+    echo "File   : $errfile\n";
+    echo "Line   : $errline\n";
+    if ($errcontext)
+        echo "Context: \n" . var_export($errcontext, TRUE);
+    exit;
+}
+
+function level1($a, $b, $c)
+{
+    trigger_error("This is an error", E_USER_ERROR);
+}
+
+set_error_handler('handler');
+echo level1(TRUE, 222, 'C');
