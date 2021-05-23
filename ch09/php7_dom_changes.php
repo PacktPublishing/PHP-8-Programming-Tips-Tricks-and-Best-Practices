3c67fe4ba8d2c67424bf6f232eef8919c2255b39
dbierer <doug@unlikelysource.com>
2021-05-23 13:30:35 +0700

    2021-05-23 DB: Added DOM examples

--- ch09/php7_dom_changes.php
+++ ch09/php7_dom_changes.php
@@ -1,12 +1,10 @@
 <?php
-// /repo/ch09/php8_dom_changes.php
+// /repo/ch09/php7_dom_changes.php
 // moves "Topic X" from $doc1 to $doc2
 
 // load both HTML files into DOM
-$doc1 = new DomDocument();
-$doc2 = new DomDocument();
-$doc1->loadHTMLFile('dom_test_1.html');
-$doc2->loadHTMLFile('dom_test_2.html');
+$doc1 = DomDocument::loadHTMLFile('dom_test_1.html');
+$doc2 = DomDocument::loadHTMLFile('dom_test_2.html');
 
 // extract topic X from doc 1
 $topic = $doc1->getElementById('X');
@@ -14,11 +12,11 @@ $topic = $doc1->getElementById('X');
 // insert topic X into doc 2
 $new = $doc2->importNode($topic);
 $new->textContent= $topic->textContent;
-$main = $doc2->getElementById('content');
-$main->append($new);
+$main  = $doc2->getElementById('content');
+$main->appendChild($new);
 
 // remove topic X from doc 1
-$topic->remove();
+$topic->parentNode->removeChild($topic);
 
 // echo after
 echo $doc1->saveHTML();
@@ -26,4 +24,4 @@ echo $doc2->saveHTML();
 
 // last element of main
 echo "\nLast Topic in Doc 2: ";
-echo $main->lastElementChild->textContent . "\n";
+echo $main->lastChild->textContent . "\n";
