31d831c68c6ce6418bcb29e7d2d8260664947cd0
dbierer <doug@unlikelysource.com>
2021-06-09 15:43:47 +0700

    2021-06-09 DB: Added weak reference example

--- ch10/php8_weak_reference.php
+++ ch10/php8_weak_reference.php
@@ -3,19 +3,15 @@
 
 $obj1 = new class () { public $name = 'Fred'; };
 $obj2 = $obj1;
+$obj3 = new class () { public $name = 'Fred'; };
+$obj4 = WeakReference::create($obj3);
 
 // if we unset $obj1, it still lives on as a Zombie inside of $obj2
-var_dump($obj1);
+var_dump($obj2);
 unset($obj1);
 var_dump($obj2);
 
-// If we create a weak reference to $obj1 and then destroy it, it doesn't go away because of $obj2:
-
-$obj1 = new class () { public $name = 'Fred'; };
-$weakref = WeakReference::create($obj1);
-$obj2 = $weakref;
-var_dump($obj1, $obj2, $weakref);
-
-var_dump($weakref->get());  // object exists
-unset($obj1);
-var_dump($weakref->get());  // object exists
+// WeakReference::get() returns the associated object
+var_dump($obj4->get());
+unset($obj3);
+var_dump($obj4->get());
