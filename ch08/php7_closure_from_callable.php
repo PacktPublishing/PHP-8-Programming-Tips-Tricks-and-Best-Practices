24b99293b3711dfe89856eeea8eab4b092919dfc
dbierer <doug@unlikelysource.com>
2021-07-09 13:27:38 +0700

    2021-07-09 DB: Updated examples for chapter 8

--- ch08/php7_closure_from_callable.php
+++ ch08/php7_closure_from_callable.php
@@ -14,3 +14,22 @@ $temp = new class() { public $class = 'Anonymous: '; };
 $doMd5->bindTo($temp);
 echo $doMd5($text) . "\n";
 var_dump($doMd5);
+
+// output:
+/*
+Warning: Cannot bind method Services\HashGen::hashToMd5() to object of class class@anonymous in /repo/ch08/php7_closure_from_callable.php on line 14
+HashGen: b335d9cb00b899bc6513ecdbb2187087
+/repo/ch08/php7_closure_from_callable.php:16:
+class Closure#2 (2) {
+  public $this =>
+  class Services\HashGen#1 (1) {
+    public $class =>
+    string(9) "HashGen: "
+  }
+  public $parameter =>
+  array(1) {
+    '$text' =>
+    string(10) "<required>"
+  }
+}
+*/
--- ch08/php8_pgsql_changes.php
+++ ch08/php8_pgsql_changes.php
@@ -3,6 +3,7 @@
 
 // before running this you need to do the following from a command prompt:
 /*
+# /etc/init.d/postgresql start
 # su postgres
 bash-4.3$ psql
 postgres=# CREATE DATABASE php8_tips;
@@ -15,10 +16,26 @@ bash-4.3$ exit
 $usr = 'php8';
 $pwd = 'password';
 $db  = pg_connect("host=localhost port=5432 dbname=php8_tips user=php8 password=password");
-$sql = "SELECT * FROM users WHERE user_name='joe'";
-$stmt = pg_query($db, $sql);
-// deprecated
-echo pg_errormessage();
-$result = pg_fetch_all($stmt);
-// no results now produce an empty array
-var_dump($result);
+if ($db !== FALSE) {
+    $sql = "SELECT * FROM users WHERE user_name='joe'";
+    $stmt = pg_query($db, $sql);
+    // deprecated
+    echo pg_errormessage();
+    $result = pg_fetch_all($stmt);
+    // no results now produce an empty array
+    var_dump($result);
+} else {
+    echo "\nPostgreSQL is not running\n";
+    echo <<<EOT
+Before running this you need to do the following from a command prompt:
+# /etc/init.d/postgresql start
+# su postgres
+bash-4.3$ psql
+postgres=# CREATE DATABASE php8_tips;
+postgres=# \c php8_tips;
+postgres=# \i /repo/sample_data/pgsql_users_create.sql
+postgres=# \q
+bash-4.3$ exit
+EOT;
+
+}
